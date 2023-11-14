05aaa85626577441dedd7455449c782c3b0f2e5e
di50089 <timo.ruppenthal@stahl-holding-saar.de>
2023-11-14 10:48:33 +0100

    Umstellen von Main auf Dependency-Injection von Jexxa, damit die Main keine new Aufrufe mehr macht.

--- src/main/java/de/shs/digitalisierung/Main.java
+++ src/main/java/de/shs/digitalisierung/Main.java
@@ -2,26 +2,21 @@ package de.shs.digitalisierung;
 
 import io.jexxa.core.JexxaMain;
 import io.jexxa.drivingadapter.rest.RESTfulRPCAdapter;
-import de.shs.digitalisierung.domain.SpieleRepository;
 import de.shs.digitalisierung.applicationservice.SpieleVerwaltung;
 import de.shs.digitalisierung.domainservice.StammdatenService;
-import de.shs.digitalisierung.infrastructure.drivenadapter.persistence.SpieleRepositoryImpl;
-import java.util.Properties;
+
+
 public class Main {
 
     @SuppressWarnings("java:S106") //Okey da Demoprojekt
     public static void main(String[] args) {
-        SpieleRepository spieleRepository = new SpieleRepositoryImpl(new Properties());
-        SpieleVerwaltung spieleVerwaltung = new SpieleVerwaltung(spieleRepository);
-
-        StammdatenService stammdatenService = new StammdatenService(spieleRepository);
-        stammdatenService.initStammdaten();
 
         var jexxaMain = new JexxaMain(Main.class);
 
         jexxaMain
                 // Bind a REST adapter to expose parts of the application
-                .bind(RESTfulRPCAdapter.class).to(spieleVerwaltung)               // Get greetings: http://localhost:7501/HelloJexxa/greetings
+                .bootstrap(StammdatenService.class).with(StammdatenService::initStammdaten)
+                .bind(RESTfulRPCAdapter.class).to(SpieleVerwaltung.class)               // Get greetings: http://localhost:7501/HelloJexxa/greetings
                 .bind(RESTfulRPCAdapter.class).to(Main.class) // Get greetings: http://localhost:7501/HelloJexxa/greetings
                 .bind(RESTfulRPCAdapter.class).to(jexxaMain.getBoundedContext())  // Get stats: http://localhost:7501/BoundedContext/isRunning
 
