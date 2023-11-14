d09de674d871feb8d1d43787d1561025b000d94f
di50089 <timo.ruppenthal@stahl-holding-saar.de>
2023-11-14 08:34:05 +0100

    Fachlichkeit mit dem Technologiestack verbunden - SpieleVerwaltung über eine Restschnittstelle nach außen zur Verfügung gestellt (inklusive Open API Support) - Die jexxaspezifischen Standartinformationen der Anwendung wurden nach außen über eine Restschnittstelle zur Verfügung gestellt (inklusive Open API Support)

--- src/main/java/de/shs/digitalisierung/Main.java
+++ src/main/java/de/shs/digitalisierung/Main.java
@@ -1,5 +1,7 @@
 package de.shs.digitalisierung;
 
+import io.jexxa.core.JexxaMain;
+import io.jexxa.drivingadapter.rest.RESTfulRPCAdapter;
 import java.util.List;
 public class Main {
     public static void main(String[] args) {
@@ -12,5 +14,15 @@ public class Main {
         spieleVerwaltung.add(ittakestwo);
         List<Spiel> alleSpiele = spieleVerwaltung.get();
         alleSpiele.forEach(element -> System.out.println(element.getName()));
+
+        var jexxaMain = new JexxaMain(Main.class);
+
+        jexxaMain
+                // Bind a REST adapter to expose parts of the application
+                .bind(RESTfulRPCAdapter.class).to(spieleVerwaltung)               // Get greetings: http://localhost:7501/HelloJexxa/greetings
+                .bind(RESTfulRPCAdapter.class).to(jexxaMain.getBoundedContext())  // Get stats: http://localhost:7501/BoundedContext/isRunning
+
+                // Run your application until Ctrl-C is pressed
+                .run();
     }
 }
