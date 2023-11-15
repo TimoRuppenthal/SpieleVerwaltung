0b0bc47a9a3295a4ec2670493b347ba841c1fb6e
di50089 <timo.ruppenthal@stahl-holding-saar.de>
2023-11-15 10:41:54 +0100

    Implementierung für das Interface VerifizierungsCodeSender erstellt

--- src/main/java/de/shs/digitalisierung/domainservice/VerifizierungsCodeSender.java
+++ src/main/java/de/shs/digitalisierung/domainservice/VerifizierungsCodeSender.java
@@ -1,6 +1,7 @@
 package de.shs.digitalisierung.domainservice;
 
 import io.jexxa.addend.applicationcore.InfrastructureService;
+import de.shs.digitalisierung.domain.EMailAdresse;
 import de.shs.digitalisierung.domain.VerifizierungsCodeVerschickt;
 @InfrastructureService
 public interface VerifizierungsCodeSender {
---
+++ src/main/java/de/shs/digitalisierung/infrastructure/drivenadapter/persistence/VerifizierungsCodeSenderImpl.java
@@ -0,0 +1,28 @@
+package de.shs.digitalisierung.infrastructure.drivenadapter.persistence;
+
+import io.jexxa.addend.infrastructure.DrivenAdapter;
+import io.jexxa.infrastructure.messaging.MessageSender;
+import de.shs.digitalisierung.domain.VerifizierungsCodeVerschickt;
+import de.shs.digitalisierung.domainservice.VerifizierungsCodeSender;
+
+import java.util.Properties;
+
+import static io.jexxa.infrastructure.MessageSenderManager.getMessageSender;
+
+@DrivenAdapter
+public class VerifizierungsCodeSenderImpl implements VerifizierungsCodeSender {
+
+
+    public static final String TOPIC = "InstrumentenVerifizierungsEmail";
+    private final MessageSender messageSender;
+
+    public VerifizierungsCodeSenderImpl(Properties properties){
+        this.messageSender = getMessageSender(VerifizierungsCodeSender.class, properties);
+    }
+    public void send(VerifizierungsCodeVerschickt verifizierungsCodeVerschickt){
+        messageSender
+                .send(verifizierungsCodeVerschickt)
+                .toTopic(TOPIC)
+                .asJson();
+    }
+}
