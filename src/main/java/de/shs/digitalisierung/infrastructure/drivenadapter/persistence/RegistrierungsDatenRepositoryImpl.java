3eba89f82cb185f4cb74e5a27b636943558d01f5
di50089 <timo.ruppenthal@stahl-holding-saar.de>
2023-11-15 10:38:08 +0100

    Implementierung für das Interface RegistrierungsDatenRepository erstellt

---
+++ src/main/java/de/shs/digitalisierung/infrastructure/drivenadapter/persistence/RegistrierungsDatenRepositoryImpl.java
@@ -0,0 +1,39 @@
+package de.shs.digitalisierung.infrastructure.drivenadapter.persistence;
+
+import io.jexxa.addend.applicationcore.Repository;
+import io.jexxa.addend.infrastructure.DrivenAdapter;
+import io.jexxa.infrastructure.RepositoryManager;
+import io.jexxa.infrastructure.persistence.repository.IRepository;
+import de.shs.digitalisierung.domain.EMailAdresse;
+import de.shs.digitalisierung.domain.RegistrierungsDaten;
+import de.shs.digitalisierung.domain.RegistrierungsDatenRepository;
+
+import java.util.List;
+import java.util.Properties;
+
+@DrivenAdapter
+public class RegistrierungsDatenRepositoryImpl implements RegistrierungsDatenRepository {
+
+    private final IRepository<RegistrierungsDaten, EMailAdresse> repository;
+
+    public RegistrierungsDatenRepositoryImpl(Properties properties){
+        this.repository = RepositoryManager.getRepository(RegistrierungsDaten.class, RegistrierungsDaten::getEMailAdresse,properties);
+    }
+    public List<RegistrierungsDaten> getAll(){
+        return repository.get();
+    }
+
+    public void remove(EMailAdresse eMailAdresse){
+        repository.remove(eMailAdresse);
+    }
+
+    public void add(RegistrierungsDaten registrierungsDaten){
+        repository.add(registrierungsDaten);
+    }
+
+    public RegistrierungsDaten get(EMailAdresse eMailAdresse) {
+
+        return repository.get(eMailAdresse).orElse(null);
+    }
+
+}
