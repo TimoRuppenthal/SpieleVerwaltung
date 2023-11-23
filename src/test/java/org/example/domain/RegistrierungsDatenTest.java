347ae587e4b78818da32415f5d0975006b2a351e
di50089 <timo.ruppenthal@stahl-holding-saar.de>
2023-11-23 09:16:59 +0100

    Es wurde ein Unit-Test erstellt mit dem geprüft wird, dass ein Verifizierungscode als UUID verschickt wird

---
+++ src/test/java/org/example/domain/RegistrierungsDatenTest.java
@@ -0,0 +1,28 @@
+package org.example.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegistrierungsDatenTest {
+    @Test
+    void testErstelleVerifizierungsCode(){
+        //Arrange
+        var emailAdresse = new EMailAdresse("rpthl@gmail.com");
+        var spielDaten = new SpielDaten("Uno", "Kartenspiel", "RenaultA5", 10);
+        var objectUnderTest = new RegistrierungsDaten(emailAdresse, spielDaten);
+        var receivedDomainEvents = new ArrayList<VerifizierungsCodeVerschickt>();
+
+        DomainEventPublisher.subscribe(VerifizierungsCodeVerschickt.class, receivedDomainEvents::add);
+
+        //Act
+        objectUnderTest.sendVerifizierungsCode();
+
+        //Assert
+        assertEquals(1, receivedDomainEvents.size());
+        //Note: A UUID actually a 128 bit value (2 long). To represent 128 bit into hex string there will be 128/4=32 char (each char is 4bit long). In string format it also contains 4 (-) that's why the length is 36.
+        assertEquals(36, receivedDomainEvents.get(0).verifizierungsCode().verifizierungsCode().length());
+    }
+}
